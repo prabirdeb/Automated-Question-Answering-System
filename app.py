88d7c5f07c29baf37149a39a931273bf6d24563a
Prabir Debnath <89520031+prabirdeb@users.noreply.github.com>
2022-09-15 19:05:07 +0530

    6th commit

--- app.py
+++ app.py
@@ -146,6 +146,22 @@ def tellme(question):
     
   return answer
 
+import base64
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+
 # Streamlit Project
 import streamlit as st # All the text cell will be displayed after this import statement
 
@@ -156,7 +172,7 @@ question=quest.title() # .title() is used to get the input question string
 
 ans = tellme(question)
 
-add_bg_from_local('drona.jpg') 
+add_bg_from_local('drona.png') 
 
 if(st.button('tellme')):   # display the ans when the submit button is clicked
   st.success(ans)
